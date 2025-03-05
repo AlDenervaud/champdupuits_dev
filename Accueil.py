b9d3b21c917590ca09a10d86b01d866f3491cd1f
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2025-03-05 10:58:15 +0100

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -45,8 +45,10 @@ st.markdown("""Sur ce site vous pourrez trouver la liste des produits de la ferm
  La liste est indicative uniquement et ne reflète pas l'état des stocks,
  il se peut que certains produits soient indisponibles.""")
  
-st.markdown("""Dans le tableau ci-dessous, cochez les produits que vous souhaitez commander et
-indiquez les quantités voulues en nombre d'unités ou en poids (kg)""")
+st.info(""""Dans le tableau ci-dessous, cochez les produits que vous souhaitez commander et
+    indiquez les quantités voulues en nombre d'unités ou en poids (kg)""",
+    icon="✍️",
+)
  
 ###############################################################################
 
