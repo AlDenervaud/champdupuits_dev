39ef2bb02b503d46896baf86055b284ada1b8b2b
Aloïs Denervaud <34124866+AlDenervaud@users.noreply.github.com>
2025-03-05 08:33:14 +0100

    d

--- Accueil.py
+++ Accueil.py
@@ -27,6 +27,7 @@ products_file_path = os.path.join(root_dir, "products.xlsx")
 # Get list of products
 df = pd.read_excel(products_file_path, sheet_name="products")
 df["Prix"] = df["Prix"].apply(lambda x: x if "/kg" in str(x).lower() else "{} €".format(x))
+df["Image_Path"] = df["Image_Path"].astype(str)
 
 # Create a grid
 gb = GridOptionsBuilder.from_dataframe(df) #, editable=True)
